264950d98eac9254f13dee930b4270581509588f
arturchique <aepremyan3993@gmail.com>
2021-07-05 12:09:29 +0300

    хероку настройки

--- practice/settings.py
+++ practice/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django-heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
 
 try:
     from .local_settings import *
